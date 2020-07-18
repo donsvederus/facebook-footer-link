16728c7147d8d9107dd97bbc681363f0ac34b3d0
donsvederus <svederus@gmail.com>
2020-07-18 17:15:26 -0600

    0.1.2.4 Plugin Basic Initial File Setup

---
+++ facebook-footer-link.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:  Facebook Footer Link
+ * Description:  Adds a Facebook profile link to the end of the post.
+ * Version:  1.0
+ * Author: Don Svederus
+ * 
+ * 
+ */
+
+
+ // Exit if Accessed Directly
+ if (!defined('ABSPATH')){
+     exit;
+ }
+
